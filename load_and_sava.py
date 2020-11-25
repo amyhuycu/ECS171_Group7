662c0cf8ed66a4fe700b36ee340b55d697be4a7f
gethshap <42168332+gethshap@users.noreply.github.com>
2020-11-25 02:35:16 -0800

    Update load_and_sava.py
    make the script received argument and run to save the result

--- load_and_sava.py
+++ load_and_sava.py
@@ -1,6 +1,8 @@
 import keras
 import os
 import json
+import sys
+import panda as pd
 
 def model_save(model):
     model.save("my_model")
@@ -15,6 +17,21 @@ def save_result(model,pars):
     with open("result.json",'w') as output_file:
         json.dump(data,output_file)
     output_file.close()
+
+if __name__ == "__main__":
+    par1 = int(sys.argv[1])
+    par2 = int(sys.argv[2])
+    
+    # .............
+    # depends on model
+    model = model_load() //pre-trained model
+    pars = pd.Series([par1, par2])
+    save_result(model,pars)
+    
+    
+    
+    
+    
 
 
 
