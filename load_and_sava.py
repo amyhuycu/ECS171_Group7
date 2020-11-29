c5f07eb8106a032f741cdf0d23b0a6154783cecb
gethshap <42168332+gethshap@users.noreply.github.com>
2020-11-28 22:35:17 -0800

    Update load_and_sava.py

--- load_and_sava.py
+++ load_and_sava.py
@@ -2,21 +2,36 @@ import keras
 import os
 import json
 import sys
-import panda as pd
+import pandas as pd
 
 def model_save(model):
-    model.save("my_model")
+    model.save("my_model.h5")
+    
+def get_model():
+    # Create a simple model.
+    inputs = keras.Input(shape=(2,))
+    outputs = keras.layers.Dense(1)(inputs)
+    model = keras.Model(inputs, outputs)
+    model.compile(optimizer="adam", loss="mean_squared_error")
+    return model
+
 
 def model_load():
-    model = keras.models.load_model("my_model")
+    model = get_model()
+    model.load_weights('my_model.h5')
     return model
 
 def save_result(model,pars):
     answer = model.predict(pars)
-    data = [{"result",str(answer)}]
+          
+          
+    data1 = {"result" : str(answer[0][0])}
+          
+          
     with open("result.json",'w') as output_file:
-        json.dump(data,output_file)
+        json.dump(data1,output_file)
     output_file.close()
+    
 
 if __name__ == "__main__":
     par1 = int(sys.argv[1])
@@ -24,9 +39,10 @@ if __name__ == "__main__":
     
     # .............
     # depends on model
-    model = model_load() //pre-trained model
-    pars = pd.Series([par1, par2])
+    model = model_load()
+    pars = pd.DataFrame([[par1, par2]])
     save_result(model,pars)
+    
     
     
     
